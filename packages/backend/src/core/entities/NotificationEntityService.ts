a50f71a1026f0a0e6d27ff2db06b6efc81f46a73
slofp <phy.public@gmail.com>
2024-06-02 19:31:26 +0900

    fix: notification role error? fixed #66

--- packages/backend/src/core/entities/NotificationEntityService.ts
+++ packages/backend/src/core/entities/NotificationEntityService.ts
@@ -141,7 +141,7 @@ export class NotificationEntityService implements OnModuleInit {
 		// #endregion
 
 		const needsRole = notification.type === 'roleAssigned';
-		const role = needsRole ? await this.roleEntityService.pack(notification.roleId) : undefined;
+		const role = needsRole ? await this.roleEntityService.pack(notification.roleId).catch(() => undefined) : undefined;
 		// if the role has been deleted, don't show this notification
 		if (needsRole && !role) {
 			return null;
